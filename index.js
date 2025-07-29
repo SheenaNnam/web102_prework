c015814d6e7153d25a656bfae7baca3515b66342
Sheena Nnam <sheenannam05@gmail.com>
2025-07-28 22:56:37 -0400

    Questions 4-6 i think?? lawl

--- index.js
+++ index.js
@@ -112,11 +112,14 @@ varNumGames.innerHTML = `${totalGames.toLocaleString('en-US')}`;
 function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
-    // use filter() to get a list of games that have not yet met their goal
+    // use filter() to get a list of games that have not yet met their goal  
 
-
+    addGamesToPage(GAMES_JSON.filter((game) =>
+    {
+        return game.pledged < game.goal;
+    }))
     // use the function we previously created to add the unfunded games to the DOM
-
+    
 }
 
 // show only games that are fully funded
@@ -124,7 +127,10 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
+    addGamesToPage(GAMES_JSON.filter((game) =>
+    {
+        return game.pledged >= game.goal;
+    }))
 
     // use the function we previously created to add unfunded games to the DOM
 
@@ -135,7 +141,7 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
-
+    addGamesToPage(GAMES_JSON)
 }
 
 // select each button in the "Our Games" section
@@ -145,6 +151,9 @@ const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
 
+unfundedBtn.addEventListener("click", filterUnfundedOnly)
+fundedBtn.addEventListener("click", filterFundedOnly)
+allBtn.addEventListener("click", showAllGames)
 
 /*************************************************************************************
  * Challenge 6: Add more information at the top of the page about the company.
@@ -155,12 +164,19 @@ const allBtn = document.getElementById("all-btn");
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
-
-
+   let numUnfunded = GAMES_JSON.reduce((init, game) => {
+        return game.pledged < game.goal ? init + 1 : init;
+    }, 0)
+    
 // create a string that explains the number of unfunded games using the ternary operator
-
+    
+    const displayStr = `A total of $${totalAmtRaised.toLocaleString('en-US')} has been raised for ${totalGames} games.
+    Currently ${numUnfunded} ${numUnfunded > 1? "games ": "game "} remain unfunded. Help us fund our super awesome cool amazing games!!! `;
 
 // create a new DOM element containing the template string and append it to the description container
+    let newPara = document.createElement("p");
+    newPara.innerHTML = displayStr;
+    document.getElementById("description-container").append(newPara);
 
 /************************************************************************************
  * Challenge 7: Select & display the top 2 games
